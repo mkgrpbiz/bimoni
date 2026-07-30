d31c7ead4ff782638dc3009da8b000a5f2ab7f46
BIMONI Dev <info@mkgrp.biz>
2026-07-30 11:05:06 +0900

    案件一覧: 応募残数列に薄いピンク背景を追加

--- resources/views/admin/campaigns/index.blade.php
+++ resources/views/admin/campaigns/index.blade.php
@@ -83,7 +83,7 @@ $tabs = [
                 <th class="px-3 py-3 text-left">PR媒体</th>
                 <th class="px-3 py-3 text-left">種別</th>
                 <th class="px-3 py-3 text-right">応募総数</th>
-                <th class="px-3 py-3 text-right">応募残数</th>
+                <th class="px-3 py-3 text-right bg-pink-50">応募残数</th>
                 <th class="px-3 py-3 text-center">回収必須</th>
                 <th class="px-3 py-3 text-right">粗利</th>
                 <th class="px-3 py-3 text-center">操作</th>
@@ -118,7 +118,7 @@ $tabs = [
                 <td class="px-3 py-3 text-gray-700">{{ $campaign->getPrMediaLabel() }}</td>
                 <td class="px-3 py-3 text-gray-700">{{ $campaign->getTypeLabel() }}</td>
                 <td class="px-3 py-3 text-right text-gray-700">{{ number_format($campaign->applications_total_count) }}件</td>
-                <td class="px-3 py-3 text-right text-gray-700">{{ number_format($campaign->applications_pending_count) }}件</td>
+                <td class="px-3 py-3 text-right text-gray-700 bg-pink-50">{{ number_format($campaign->applications_pending_count) }}件</td>
                 <td class="px-3 py-3 text-center">
                     @if($campaign->collection_requirement === '回収必須')
                         <span class="text-red-600 font-medium">必須</span>
