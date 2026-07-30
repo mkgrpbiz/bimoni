fd4ea3439c330f5bb28c54e32915ab2c742a238a
BIMONI Dev <info@mkgrp.biz>
2026-07-30 11:01:43 +0900

    案件一覧: 応募総数/応募残数/回収必須/粗利の列幅・配置を調整して間隔を均等に
    列幅が内容に応じて自動調整され、種別との間が広く空く一方で応募総数・応募残数が
    くっついて見える不均等なレイアウトになっていたため、各列に最小幅を指定。
    回収必須も応募残数と粗利の中間に見えるよう中央寄せに変更。

--- resources/views/admin/campaigns/index.blade.php
+++ resources/views/admin/campaigns/index.blade.php
@@ -82,10 +82,10 @@ $tabs = [
                 <th class="px-3 py-3 text-left">ステータス</th>
                 <th class="px-3 py-3 text-left">PR媒体</th>
                 <th class="px-3 py-3 text-left">種別</th>
-                <th class="px-3 py-3 text-right">応募総数</th>
-                <th class="px-3 py-3 text-right">応募残数</th>
-                <th class="px-3 py-3 text-left">回収必須</th>
-                <th class="px-3 py-3 text-right">粗利</th>
+                <th class="px-3 py-3 text-right w-24 whitespace-nowrap">応募総数</th>
+                <th class="px-3 py-3 text-right w-24 whitespace-nowrap">応募残数</th>
+                <th class="px-3 py-3 text-center w-20 whitespace-nowrap">回収必須</th>
+                <th class="px-3 py-3 text-right w-24 whitespace-nowrap">粗利</th>
                 <th class="px-3 py-3 text-center">操作</th>
             </tr>
         </thead>
@@ -119,7 +119,7 @@ $tabs = [
                 <td class="px-3 py-3 text-gray-700">{{ $campaign->getTypeLabel() }}</td>
                 <td class="px-3 py-3 text-right text-gray-700">{{ number_format($campaign->applications_total_count) }}件</td>
                 <td class="px-3 py-3 text-right text-gray-700">{{ number_format($campaign->applications_pending_count) }}件</td>
-                <td class="px-3 py-3">
+                <td class="px-3 py-3 text-center">
                     @if($campaign->collection_requirement === '回収必須')
                         <span class="text-red-600 font-medium">必須</span>
                     @else
